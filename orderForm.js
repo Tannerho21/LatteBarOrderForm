459644f09d3368babb4c88d660555b30e0d3a861
Tannerho21 <tannerHo21@gmail.com>
2021-02-10 10:18:10 -0500

    update 1.0
    Added files for each query parameter. and updated them to the minimum requirement.

--- orderForm.js
+++ orderForm.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/orderForm', (req, res) => {
-  res.send('Testing')
+app.get('/', (req, res) => {
+  res.send('Please use either /orderForm or /currentOrders depending onyou needs')
 })
 
+app.get('/orderForm', (req, res) => {
+  res.sendFile(__dirname + '/orderForm.html')
+})
+app.get('/currentOrders', (req, res) => {
+  res.sendFile(__dirname + '/currentOrders.html')
+})
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`latte app listening at http://localhost:${port}`)
 })
