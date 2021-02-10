caffbaa5fb3c4568d95073bef7e8916dba5b2e30
Tannerho21 <tannerHo21@gmail.com>
2021-02-10 09:19:40 -0500

    Core

--- orderForm.js
+++ orderForm.js
@@ -1,1 +1,11 @@
-var = 
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/orderForm', (req, res) => {
+  res.send('Testing')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
